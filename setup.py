537689cf87c774d927fa4d84dba5d38b43e10267
tiran79 <tiran79@6b6e1e4a-9bc0-11dd-a4cb-5bfb67625b15>
2008-10-22 18:54:41 +0000

    Setup tweaks Added MANIFEST.in Optionally use setuptools
    git-svn-id: http://python-multiprocessing.googlecode.com/svn/trunk@12 6b6e1e4a-9bc0-11dd-a4cb-5bfb67625b15

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-# Autodetecting setup.py script for building the Python extensions
-#
-
 __version__ = "$Revision$"
 
 import os
 import sys
 import glob
 
-from distutils.core import setup, Extension
+try:
+    from setuptools import setup, Extension
+except ImportError:
+    from distutils.core import setup, Extension
+
 
 if sys.version_info < (2, 4):
     raise ValueError("Versions of Python before 2.4 are not supported")
@@ -15,7 +16,6 @@ if sys.version_info < (2, 4):
 if sys.platform == 'win32': # Windows
     macros = dict()
     libraries = ['ws2_32']
-
 elif sys.platform.startswith('darwin'): # Mac OSX
     macros = dict(
         HAVE_SEM_OPEN=1,
@@ -24,7 +24,6 @@ elif sys.platform.startswith('darwin'): # Mac OSX
         HAVE_BROKEN_SEM_GETVALUE=1
         )
     libraries = []
-
 elif sys.platform.startswith('cygwin'): # Cygwin
     macros = dict(
         HAVE_SEM_OPEN=1,
@@ -33,7 +32,6 @@ elif sys.platform.startswith('cygwin'): # Cygwin
         HAVE_BROKEN_SEM_UNLINK=1
         )
     libraries = []
-
 elif sys.platform in ('freebsd5', 'freebsd6', 'freebsd7', 'freebsd8'):
     # FreeBSD's P1003.1b semaphore support is very experimental
     # and has many known problems. (as of June 2008)
@@ -43,7 +41,6 @@ elif sys.platform in ('freebsd5', 'freebsd6', 'freebsd7', 'freebsd8'):
         HAVE_FD_TRANSFER=1,
         )
     libraries = []
-
 elif sys.platform.startswith('openbsd'):
     macros = dict(                  # OpenBSD
         HAVE_SEM_OPEN=0,            # Not implemented
@@ -51,7 +48,6 @@ elif sys.platform.startswith('openbsd'):
         HAVE_FD_TRANSFER=1,
         )
     libraries = []
-
 else:                                   # Linux and other unices
     macros = dict(
         HAVE_SEM_OPEN=1,
@@ -60,6 +56,7 @@ else:                                   # Linux and other unices
         )
     libraries = ['rt']
 
+
 if sys.platform == 'win32':
     multiprocessing_srcs = ['Modules/_multiprocessing/multiprocessing.c',
                             'Modules/_multiprocessing/semaphore.c',
@@ -67,7 +64,6 @@ if sys.platform == 'win32':
                             'Modules/_multiprocessing/socket_connection.c',
                             'Modules/_multiprocessing/win32_functions.c'
                            ]
-
 else:
     multiprocessing_srcs = ['Modules/_multiprocessing/multiprocessing.c',
                             'Modules/_multiprocessing/socket_connection.c'
@@ -77,11 +73,12 @@ else:
         multiprocessing_srcs.append('Modules/_multiprocessing/semaphore.c')
 
 
-print 'Macros:'
-for name, value in sorted(macros.iteritems()):
-    print '\t%s = %r' % (name, value)
-print '\nLibraries:\n\t%r\n' % ', '.join(libraries)
-print '\Sources:\n\t%r\n' % ', '.join(multiprocessing_srcs)
+if 0:
+    print 'Macros:'
+    for name, value in sorted(macros.iteritems()):
+        print '\t%s = %r' % (name, value)
+    print '\nLibraries:\n\t%r\n' % ', '.join(libraries)
+    print '\Sources:\n\t%r\n' % ', '.join(multiprocessing_srcs)
 
 
 extensions = [
@@ -90,14 +87,15 @@ extensions = [
               define_macros=macros.items(),
               include_dirs=["Modules/_multiprocessing"],
               depends=(glob.glob('Modules/_multiprocessing/*.h') +
-                       glob.glob('Modules/_multiprocessing/*.c') + 
                        ['setup.py'])
               ),
     ]
 
 if sys.version_info < (2, 5):
+    # Python 2.4's mmap doesn't support anonymous memory
     extensions.append(
-        Extension('multiprocessing._mmap25', sources=["Modules/mmapmodule.c"])
+        Extension('multiprocessing._mmap25', 
+	          sources=["Modules/mmapmodule.c"])
         )
 
 packages = [
@@ -107,14 +105,14 @@ packages = [
 
 package_dir = {
     'multiprocessing': 'Lib/multiprocessing',
-    #'processing.doc': 'doc',
-    #'processing.tests': 'tests',
+    #'multiprocessing.doc': 'Doc/library',
+    #'multiprocessing.tests': 'Lib/test',
     #'processing.examples': 'examples'
     }
 
 setup(
     name='multiprocessing',
-    version="XXX",
+    version="0.0-2.6.0",
     description=('Package for using processes which mimics ' +
                  'the threading module'),
     #long_description=long_description,
